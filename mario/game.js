5401688032056ae3014401b2f7e8e7110a0b01aa
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2021-08-12 23:03:47 +0800

    add sprites

--- mario/game.js
+++ mario/game.js
@@ -6,25 +6,25 @@ kaboom({
   clearColor: [0, 0, 0, 1], // black background
 });
 
-loadRoot("https://i.imgur.com/");
-loadSprite("coin", "wbKxhcd.png");
-loadSprite("evil-shroom", "KPO3fR9.png");
-loadSprite("brick", "pogC9x5.png");
-loadSprite("block", "M6rwarW.png");
-loadSprite("mario", "Wb1qfhK.png");
-loadSprite("mushroom", "0wMd92p.png");
-loadSprite("surprise", "gesQ1KP.png");
-loadSprite("unboxed", "bdrLpi6.png");
-loadSprite("pipe-top-left", "ReTPiWY.png");
-loadSprite("pipe-top-right", "hj2GK4n.png");
-loadSprite("pipe-bottom-left", "c1cYSbt.png");
-loadSprite("pipe-bottom-right", "nqQ79eI.png");
+// loadRoot("https://i.imgur.com/");
+loadSprite("coin", "sprites/wbKxhcd.png");
+loadSprite("evil-shroom", "sprites/KPO3fR9.png");
+loadSprite("brick", "sprites/pogC9x5.png");
+loadSprite("block", "sprites/M6rwarW.png");
+loadSprite("mario", "sprites/Wb1qfhK.png");
+loadSprite("mushroom", "sprites/0wMd92p.png");
+loadSprite("surprise", "sprites/gesQ1KP.png");
+loadSprite("unboxed", "sprites/bdrLpi6.png");
+loadSprite("pipe-top-left", "sprites/ReTPiWY.png");
+loadSprite("pipe-top-right", "sprites/hj2GK4n.png");
+loadSprite("pipe-bottom-left", "sprites/c1cYSbt.png");
+loadSprite("pipe-bottom-right", "sprites/nqQ79eI.png");
 
-loadSprite("blue-block", "fVscIbn.png");
-loadSprite("blue-brick", "3e5YRQd.png");
-loadSprite("blue-steel", "gqVoI2b.png");
-loadSprite("blue-evil-shroom", "SvV4ueD.png");
-loadSprite("blue-surprise", "RMqCc1G.png");
+loadSprite("blue-block", "sprites/fVscIbn.png");
+loadSprite("blue-brick", "sprites/3e5YRQd.png");
+loadSprite("blue-steel", "sprites/gqVoI2b.png");
+loadSprite("blue-evil-shroom", "sprites/SvV4ueD.png");
+loadSprite("blue-surprise", "sprites/RMqCc1G.png");
 
 // add scenes. default
 scene("game", () => {
